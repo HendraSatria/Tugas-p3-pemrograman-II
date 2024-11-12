e4a4e7df0614b4fc41e78037e30444fd12d8442b
M hendra satria <162526343+HendraSatria@users.noreply.github.com>
2024-11-12 22:43:32 +0700

    Add files via upload

---
+++ Aplikasi PBO/Pemanggilan hasill/Pemanggilan2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tugas2;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Pemanggilan2 {
+
+    public static void main(String[] args) {
+        // Memanggil metode prosesPenilaian dari kelas PenilaianSiswa
+        Penilaian_Siswa.prosesPenilaian("Budi", 85);
+        Penilaian_Siswa.prosesPenilaian("Siti", 65);
+        Penilaian_Siswa.prosesPenilaian("Hendra", 80);
+    }
+}
